97520910d19231aa5e5df0a5398ba616c1d127ec
FourLeafCoder <pwhitedeveloper@gmail.com>
2018-12-25 12:59:48 -0500

    sql path for testing

--- src/weeb/data/APIKeys.java
+++ src/weeb/data/APIKeys.java
@@ -11,7 +11,7 @@ import org.sqlite.SQLiteException;
 public class APIKeys {
 
 	private static final String DB_NAME = "Anilert.db";
-	private static final String CONNECTION_STRING = "jdbc:sqlite:" + DB_NAME;
+	private static final String CONNECTION_STRING = "jdbc:sqlite:/home/leafcoder/SQL/" + DB_NAME;
 	
 	
 	private static final String TABLE_API_KEYS = "API_KEYS";
--- src/weeb/power/Main.java
+++ src/weeb/power/Main.java
@@ -4,14 +4,6 @@ import weeb.data.APIKeys;
 
 public class Main {
 
-	public static final String DB_NAME = "testjava.db";
-	public static final String CONNECTION_STRING = "jdbc:sqlite:/home/leafcoder/eclipse-workspace/TestDB/" + DB_NAME;
-	
-	public static final String TABLE_CONTACTS = "contacts";
-	
-	public static final String COLUMN_NAME = "name";
-	public static final String COLUMN_PHONE = "phone";
-	public static final String COLUMN_EMAIL = "email";
 	
 	public static void main(String[] args) {
 
